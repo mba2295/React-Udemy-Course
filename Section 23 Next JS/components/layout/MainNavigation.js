b3a2a74ee3aa0db602b86343b1d7c9f7d4b4d5c7
Muhammad Bilal <muhammdbilalansari@gmail.com>
2022-06-22 21:20:30 +0500

    Update MainNavigation.js

--- Section 23 Next JS/components/layout/MainNavigation.js
+++ Section 23 Next JS/components/layout/MainNavigation.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 function MainNavigation() {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>React Vercel CICD Meetups</div>
+      <div className={classes.logo}>CICD NEXT Meetups</div>
       <nav>
         <ul>
           <li>
